1bc95d35c3382983eaa17e4dda2ecd742a70d3ca
Jebaitedneko <kannanpulimoottil@gmail.com>
2019-03-04 15:46:26 +0530

    Update DifficultyCalculator.java

--- src/main/java/lt/ekgame/beatmap_analyzer/difficulty/DifficultyCalculator.java
+++ src/main/java/lt/ekgame/beatmap_analyzer/difficulty/DifficultyCalculator.java
@@ -9,6 +9,6 @@ public interface DifficultyCalculator {
 	
 	public Difficulty calculate(Mods mods, Beatmap beatmap);
 	
-	public double calculateDifficulty(List<Double> strains);
+	public double calculateDifficulty(List<Float> strains);
 
 }
